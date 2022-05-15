9d388c2a85f75f080e16326bbc29e56a5cae7b17
Tom Booker <30446949+thomas-booker@users.noreply.github.com>
2022-05-15 13:07:32 +0100

    Added basic models and package for raidplanner business logic

---
+++ src/main/java/uk/co/thomasbooker/model/Trial.java
@@ -0,0 +1,14 @@
+package uk.co.thomasbooker.model;
+
+import java.time.LocalDateTime;
+
+public class Trial {
+
+    String ID;
+
+    String name;
+
+    String owner;
+
+    LocalDateTime startTime;
+}
---
+++ src/main/java/uk/co/thomasbooker/model/TrialMember.java
@@ -0,0 +1,10 @@
+package uk.co.thomasbooker.model;
+
+public class TrialMember {
+
+    String ID;
+
+    String name;
+
+    String role;
+}

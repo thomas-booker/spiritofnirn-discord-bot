d3c6e78d5c95fcea02d94135d1672d3a3bb77d9a
Tom Booker <30446949+thomas-booker@users.noreply.github.com>
2022-05-16 08:53:23 +0100

    Adding Raidplanner service, CreateTrial

--- src/main/java/uk/co/thomasbooker/service/raidplanner/CreateTrial.java
+++ src/main/java/uk/co/thomasbooker/service/raidplanner/CreateTrial.java
@@ -1,14 +1,12 @@
 package uk.co.thomasbooker.service.raidplanner;
 
-import org.hibernate.service.spi.InjectService;
 import uk.co.thomasbooker.model.Trial;
 import uk.co.thomasbooker.repository.TrialRepository;
 
 public class CreateTrial {
 
     TrialRepository trialRepository;
-
-    @InjectService
+    
     public void execute(Trial trial) {
         if (!exists(trial.getId())) {
             trialRepository.save(trial);
